764f45d19f7f256ea65f7d9ef8a4b7468f6ed5cb
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-31 23:52:20 +0300

    Update main.cmd

--- main.cmd
+++ main.cmd
@@ -5,9 +5,9 @@ cscript slmgr.vbs /skms kms.digiboy.ir:1688
 start "https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lang=en-US"
 start "https://drivers.pantum.ru/userfiles/files/download/%E9%A9%B1%E5%8A%A8%E6%96%87%E4%BB%B6/2013/2024%E5%B9%B48%E6%9C%887%E6%97%A5%202013/Pantum%20P2200-P2500-P2600-S2000%20Series%20Windows%20Driver%20V2_6_38.exe"
 pause
-start "C:\Users\govno\Downloads\Firefox Installer.exe"
-start "C:\Users\govno\Downloads\Pantum P2200-P2500-P2600-S2000 Series Windows Driver V2_6_38.exe"
+start "%CSIDL_DEFAULT_DOWNLOADS%\Firefox Installer.exe"
+start "%CSIDL_DEFAULT_DOWNLOADS%\Pantum P2200-P2500-P2600-S2000 Series Windows Driver V2_6_38.exe"
 pause
 
 taskkill /F /IM msedge.exe /T
-rmdir /S /Q "C:\Windows\Programm Files\Microsoft"
+rmdir /S /Q "%PROGRAMFILES%\Microsoft"
