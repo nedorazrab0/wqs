10d8e8e64d9915fe7e65c16c39cc1132bc689809
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-31 23:39:33 +0300

    Create main.cmd

---
+++ main.cmd
@@ -0,0 +1,13 @@
+@echo off
+cscript slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+cscript slmgr.vbs /skms kms.digiboy.ir:1688
+
+start "https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lang=en-US"
+start "https://drivers.pantum.ru/userfiles/files/download/%E9%A9%B1%E5%8A%A8%E6%96%87%E4%BB%B6/2013/2024%E5%B9%B48%E6%9C%887%E6%97%A5%202013/Pantum%20P2200-P2500-P2600-S2000%20Series%20Windows%20Driver%20V2_6_38.exe"
+pause
+start "C:\Users\govno\Downloads\Firefox Installer.exe"
+start "C:\Users\govno\Downloads\Pantum P2200-P2500-P2600-S2000 Series Windows Driver V2_6_38.exe"
+pause
+
+taskkill /F /IM msedge.exe /T
+rmdir /S /Q "C:\Windows\Programm Files\Microsoft"
