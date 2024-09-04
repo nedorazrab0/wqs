23fafb7d30ab14a07ba5f50e596c49d36b7c122c
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-09-04 20:29:19 +0300

    Update runasadmin.cmd

--- runasadmin.cmd
+++ runasadmin.cmd
@@ -1,5 +1,8 @@
-cscript C:\Windows\System32\slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
-cscript C:\Windows\System32\slmgr.vbs /skms kms.digiboy.ir:1688
+@echo off
+cscript slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+cscript slmgr.vbs /skms kms.digiboy.ir:1688
+cscript slmgr.vbs /ato
 
 taskkill /F /IM msedge.exe /T
-rmdir /Q /S "C:\Program Files\Microsoft"
+rmdir /Q /S "%PROGRAMFILES%\Microsoft"
+shutdown /s
