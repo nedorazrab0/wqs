303deb05c8b6c0a11694097ef83946f6caddc897
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-09-04 20:12:08 +0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,19 +1,3 @@
-param([switch]$Elevated)
-
-function Test-Admin {
-    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-
-if ((Test-Admin) -eq $false)  {
-    if ($elevated) {
-        # tried to elevate, did not work, aborting
-    } else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-    }
-    exit
-}
-
 cscript C:\Windows\System32\slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
 cscript C:\Windows\System32\slmgr.vbs /skms kms.digiboy.ir:1688
 
@@ -22,7 +6,3 @@ explorer "https://drivers.pantum.ru/userfiles/files/download/%E9%A9%B1%E5%8A%A8%
 pause
 start "C:\Users\govno\Downloads\Firefox Installer.exe"
 start "C:\Users\govno\Downloads\Pantum P2200-P2500-P2600-S2000 Series Windows Driver V2_6_38.exe"
-pause
-
-Stop-Process -Force -Name msedge
-rm -r -Force "C:\Program Files\Microsoft"
