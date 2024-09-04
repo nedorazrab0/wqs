a4eaabc034d8a25bc923203d71a556771644a25f
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-09-04 20:09:40 +0300

    Update runasadmin.cmd

--- runasadmin.cmd
+++ runasadmin.cmd
@@ -1,1 +1,5 @@
-powershell Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+cscript C:\Windows\System32\slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+cscript C:\Windows\System32\slmgr.vbs /skms kms.digiboy.ir:1688
+
+taskkill /F /IM msedge.exe /T
+rmdir /Q /S "C:\Program Files\Microsoft"
