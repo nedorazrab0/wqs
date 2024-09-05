3cfaac48469dcc72293a03a8b6884b12df40f636
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-09-05 17:44:03 +0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -2,4 +2,3 @@ start "https://download.mozilla.org/?product=firefox-stub&os=win&lang=en-US"
 start "https://drivers.pantum.ru/userfiles/files/download/%E9%A9%B1%E5%8A%A8%E6%96%87%E4%BB%B6/2013/2024%E5%B9%B48%E6%9C%887%E6%97%A5%202013/Pantum%20P2200-P2500-P2600-S2000%20Series%20Windows%20Driver%20V2_6_38.exe"
 pause
 Invoke-Item "C:\Users\govno\Downloads\Firefox Installer.exe"
-Invoke-Item "C:\Users\govno\Downloads\Pantum P2200-P2500-P2600-S2000 Series Windows Driver V2_6_38.exe"
